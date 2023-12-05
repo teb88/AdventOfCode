c573a0da0ca7fbb3b886bbe21ec631ccaf76098a
Esteban Gomez <estebanrc88@gmail.com>
2023-12-05 17:12:18 -0300

    add day 1 part 1 in GO

---
+++ 2023/days/1/go/part1.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+
+func main() {
+  input := readInputFile();
+	lines := strings.Split(input, "\n")
+
+	sum := 0
+
+	for _, line := range lines {
+		value := restoreValue(line)
+		fmt.Println(value, "from", line)
+		sum += value
+	}
+
+	fmt.Println("RESULT:")
+	fmt.Println(sum)
+}
+
+
+func restoreValue(entry string) int {
+	r := regexp.MustCompile(`(\d)`)
+	matches := r.FindAllString(entry, -1)
+
+	if len(matches) > 0 {
+		value, err1 := strconv.Atoi(matches[0] + matches[len(matches) - 1])
+	
+		if err1 != nil {
+			return 0;
+		}
+
+		return value 
+	} else {
+		return 0;
+	}
+}
+
+func readInputFile() string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+			panic(err)
+	}
+
+	return string(data)
+}
