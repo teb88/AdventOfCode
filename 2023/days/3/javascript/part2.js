bb48a0120b8885724ef556d382eb12885c3e6dae
Esteban Gomez <estebanrc88@gmail.com>
2023-12-11 22:55:47 -0300

    add day 3 part 2 js

--- 2023/days/3/javascript/part2.js
+++ 2023/days/3/javascript/part2.js
@@ -6,31 +6,74 @@ const input = require("fs").readFileSync(
 
 const lines = input.split("\n");
 
+/**
+ * coordinates where an adjacent * was found
+ * value is an array with the adjacent values
+ */
+const coordinates = new Map();
+
 console.time("run");
-const result = lines.reduce((acc, line, index) => {
-  return acc + evalLine(line, index);
-}, 0);
+
+// iterate over the lines
+lines.forEach((line, index) => {
+  evalLine(line, index);
+});
+
+let result = 0;
+
+coordinates.forEach((value, key) => {
+  if (value.length === 2) {
+    result += value[0] * value[1];
+  }
+});
+
 console.timeEnd("run");
 
-console.log(result);
+console.log("RESULT: " + result);
 
 function evalLine(line, index) {
   const matchesNumbers = line.matchAll(/(\d+)/g);
 
-  let lineSum = 0;
-
   for (const match of Array.from(matchesNumbers)) {
-    const isValid = isNumberValid({
-      value: match[0],
+    findAdjacentAsterisk({
+      value: parseInt(match[0]),
       indexStart: match.index,
       indexEnd: match.index + match[0].length,
       lineNumber: index,
     });
-    if (isValid) {
-      lineSum += parseInt(match[0]);
+  }
+}
+
+function findAdjacentAsterisk({value, indexStart, indexEnd, lineNumber}) {
+  const linesToScan = [];
+  const start = Math.max(0, indexStart - 1);
+  const end = Math.min(indexEnd + 1, lines[lineNumber].length - 1);
+
+  if (lineNumber >= 1) {
+    linesToScan.push(lineNumber - 1);
+  }
+
+  linesToScan.push(lineNumber);
+
+  if (lineNumber < lines.length - 1) {
+    linesToScan.push(lineNumber + 1);
+  }
+
+  const occurrences = [];
+  for (let i = 0; i < linesToScan.length; i++) {
+    for (let pos = start; pos < end; pos++) {
+      if (lines[linesToScan[i]].charAt(pos) === "*") {
+        occurrences.push({index: pos, line: linesToScan[i]});
+      }
     }
   }
 
-  return lineSum;
+  occurrences.forEach(({index, line}) => {
+    const coordName = `${line}x${index}`;
+    if (coordinates.has(coordName)) {
+      coordinates.get(coordName).push(value);
+    } else {
+      coordinates.set(coordName, [value]);
+    }
+  });
 }
-// wip
